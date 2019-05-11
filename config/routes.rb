4a6a7bf9d16428679a125876e2ff80f71d2fc578
Савелий Вепрев <veprev@MacBook-Pro-Savelij.local>
2019-05-11 15:26:32 +0500

    Добавил вьюхи для редактирования пользователей из админки

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'dashboard#index'
 
   resources :ticket_conversations, only: [:show, :update, :create, :new, :edit]
+  resources :users, only: [:index, :show, :update, :create, :new, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
