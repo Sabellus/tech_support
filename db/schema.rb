fc4c0739e6044e2a73f853831b6f7821f7a30708
Савелий Вепрев <veprev@MacBook-Pro-Savelij.local>
2019-05-11 15:44:05 +0500

    Добавил дефолтную роль для новых пользователей

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_235729) do
+ActiveRecord::Schema.define(version: 2019_05_11_102717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_04_26_235729) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "role"
+    t.integer "role", default: 1
     t.string "first_name"
     t.string "last_name"
     t.integer "gender"
